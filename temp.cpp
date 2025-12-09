75a61f6709756992cc33139d7919914568060eaa
Sahil <sahilkr789065@gmail.com>
2025-12-09 16:04:31 +0530

    Adding factorial functions in all files

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int factorial(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
 int main(){
     cout << "Hello World!";
 }
--- temp.js
+++ temp.js
@@ -1,1 +1,8 @@
+const factorial = (n)=>{
+    if(n==1){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
 console.log("Hello World!");
